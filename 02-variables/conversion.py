ed16d521dcde719491d18615b842f3f6f167681c
Nomfanelo Precious Vilane <precious.vilane0@gmail.com>
2026-04-17 22:06:55 +0200

    Add files via upload

---
+++ 02-variables/conversion.py
@@ -0,0 +1,24 @@
+# Declaring numbers
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+float0 = print(type(num1))
+interger0 = print(type(num2))
+integer2 = print(type(num3))
+string = print(type(string1))
+
+# converting
+# num1 into an integer
+num1 = int(num1)
+print(num1)
+# num2 into a float
+num2 = float(num2)
+print(num2)
+# num3 into a string
+num3 = str(num3)
+print(num3)
+# string1 into an integer
+string1 = int(string1)
+print(string1)
