59547319dfeee82ac621210d44fa7f0db40e1091
Nomfanelo Precious Vilane <precious.vilane0@gmail.com>
2026-04-17 22:06:30 +0200

    Add files via upload

---
+++ 01-first-computer-programe-task/hello_world.py
@@ -0,0 +1,17 @@
+# 1. Prompt the user for their username using input() function
+# 2. print out the username
+# 3. Prompt the user for their age using input() function then
+# 4. print it out age
+# 5. Finally, print the string “Hello, World!” on a new line
+
+
+# Prompt the user for their username
+username = input("Please enter your name: ")
+print(f"Your name is {username}")
+
+# Prompt the user for their age
+age = input("Please enter your age: ")
+print(f"Your age is {age}")
+
+# Print "Hello, World!"
+print("Hello, World!")
