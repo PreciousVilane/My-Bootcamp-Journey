f1aabb556e99fb87d9918fdac5d1d1cde36fa6fe
Nomfanelo Precious Vilane <precious.vilane0@gmail.com>
2026-05-12 19:10:09 +0200

    part 2

---
+++ 02-variables/02-variables/details.py
@@ -0,0 +1,10 @@
+# prompt the user fir a name, age, house number, and street name
+# print out a single sentence cointsining all the details of the user
+
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+house_number = input("Please enter your house number: ")
+street_name = input("Please enter your street name: ")
+
+print(f"Your name is {name}, you are {age} years old,"
+      f" and you live in house {house_number}, on {street_name} street.")
